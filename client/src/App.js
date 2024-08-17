4eb7bcad9adf17106fa37c1701a412886897cdec
Geetanshu Garg <geetanshugarg39@gmail.com>
2024-08-17 16:09:51 +0530

    refactored the code

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,17 @@
-import {BrowserRouter,Routes,Route} from "react-router-dom"
-import Home from "./pages/home";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Layout from "./Layout";
 
 function App() {
   return (
     <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<Home/>} />
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout/>}>
+          <Route index element={<Home />} />
+          <Route path="/login" element={<Login />} />
+        </Route>
+      </Routes>
     </BrowserRouter>
   );
 }
